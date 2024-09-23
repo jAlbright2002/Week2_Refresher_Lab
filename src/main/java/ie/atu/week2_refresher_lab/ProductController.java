441fe2644e4abd59bcf8b8f7368076661d5c6d43
jamesA <G00379271@atu.ie>
2024-09-23 16:18:45 +0100

    Populate controller with requests

--- src/main/java/ie/atu/week2_refresher_lab/ProductController.java
+++ src/main/java/ie/atu/week2_refresher_lab/ProductController.java
@@ -1,8 +1,25 @@
 package ie.atu.week2_refresher_lab;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
+
+    @GetMapping("/products")
+    public List<Product> getProducts() {
+        //logic to get products
+        return;
+    }
+
+    @PostMapping("/product")
+    public Product addProduct(@RequestBody Product product) {
+        //logic to add products
+        return product;
+    }
 
 }
