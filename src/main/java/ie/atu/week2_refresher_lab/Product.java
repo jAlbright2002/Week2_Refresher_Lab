f4b55c944cfb7d7bdbc1f7e96e9b428444601ea2
jamesA <G00379271@atu.ie>
2024-09-23 17:12:05 +0100

    Add validation dependency Add constraints to properties in Product Validate the data coming back in the body

--- src/main/java/ie/atu/week2_refresher_lab/Product.java
+++ src/main/java/ie/atu/week2_refresher_lab/Product.java
@@ -1,5 +1,8 @@
 package ie.atu.week2_refresher_lab;
 
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
+
     private Long id;
+    @NotNull
     private String name;
+    @PositiveOrZero
     private double price;
 }
