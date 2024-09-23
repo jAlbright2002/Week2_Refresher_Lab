3425796cfadcc89e031ddf0dfac7831646c2c88e
jamesA <G00379271@atu.ie>
2024-09-23 16:42:53 +0100

    Connected the ProductService to the Controller to handle the logic

--- src/main/java/ie/atu/week2_refresher_lab/ProductController.java
+++ src/main/java/ie/atu/week2_refresher_lab/ProductController.java
@@ -1,5 +1,6 @@
 package ie.atu.week2_refresher_lab;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,15 +11,22 @@ import java.util.List;
 @RestController
 public class ProductController {
 
+    private final ProductService service;
+
+    @Autowired
+    ProductController(ProductService service) {
+        this.service = service;
+    }
+
     @GetMapping("/products")
     public List<Product> getProducts() {
-        //logic to get products
-        return;
+        return service.getProducts();
     }
 
     @PostMapping("/product")
     public Product addProduct(@RequestBody Product product) {
         //logic to add products
+        service.addProduct(product);
         return product;
     }
 
