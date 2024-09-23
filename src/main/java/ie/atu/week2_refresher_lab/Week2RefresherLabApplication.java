174326827612263a5bb305c96f4fb40a8b682ca1
jamesA <G00379271@atu.ie>
2024-09-23 15:56:16 +0100

    New Project

---
+++ src/main/java/ie/atu/week2_refresher_lab/Week2RefresherLabApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week2_refresher_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2RefresherLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week2RefresherLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week2_refresher_lab/Week2RefresherLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week2_refresher_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2RefresherLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
