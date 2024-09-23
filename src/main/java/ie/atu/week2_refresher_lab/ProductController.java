f4d0e9aba3a1bcd00b96411deca821391ba95c5f
jamesA <G00379271@atu.ie>
2024-09-23 16:46:11 +0100

    Clean up imports

--- src/main/java/ie/atu/week2_refresher_lab/ProductController.java
+++ src/main/java/ie/atu/week2_refresher_lab/ProductController.java
@@ -1,10 +1,7 @@
 package ie.atu.week2_refresher_lab;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
