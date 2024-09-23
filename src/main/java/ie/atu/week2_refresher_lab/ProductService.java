82cbca6b57e963282205a1d66305f495f9414759
jamesA <G00379271@atu.ie>
2024-09-23 16:21:17 +0100

    Create product service class and add logic

---
+++ src/main/java/ie/atu/week2_refresher_lab/ProductService.java
@@ -0,0 +1,21 @@
+package ie.atu.week2_refresher_lab;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private List<Product> productList = new ArrayList<>();
+
+    private List<Product> getProducts() {
+        return productList;
+    }
+
+    public Product addProduct(Product product) {
+        productList.add(product);
+        return product;
+    }
+}
